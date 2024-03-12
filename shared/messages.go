002d8abb14c0e1c46f084ae9ac500144541a4308
Fabriciope <fabricioalvespa@gmail.com>
2024-03-12 18:06:29 -0300

    feat: add shared package

---
+++ shared/messages.go
@@ -0,0 +1,27 @@
+package shared
+
+const (
+	LoginActionName            = "login"
+	LogoutActionName           = "logout"
+	GetUsersActionName         = "users"
+	GetUsersCountActionName    = "users_count"
+	SendMessageActionName      = "send_message"
+	NewMessageNotificationName = "new_message_notification"
+	NewClientNotificationName  = "new_client_notification"
+)
+
+type Request struct {
+	Name    string `json:"name"`
+	Payload string `json:"payload,omitempty"`
+}
+
+type Response struct {
+	Name    string      `json:"name"`
+	Err     bool        `json:"error"`
+	Payload interface{} `json:"payload"`
+}
+
+type TextMessage struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
