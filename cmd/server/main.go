e91844ec44120d0a6cf516cce516a42bc746dac7
crgimenes <crg@crg.eti.br>
2024-04-23 23:22:18 -0300

    add flags

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/Fabriciope/cli_chat/server"
 )
 
-const (
-	ip   = "localhost"
-	port = 5000
-)
-
 func main() {
+	var (
+		ip   = "0.0.0.0"
+		port = 5000
+	)
+
+	flag.StringVar(&ip, "ip", ip, "IP address to listen on")
+	flag.IntVar(&port, "port", port, "Port to listen on")
+
+	flag.Parse()
+
 	server, err := server.NewTCPServer(ip, port)
 	if err != nil {
 		log.Panic(err)
