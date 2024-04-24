90098bf71c626e69bff1119002944e1d4852447a
crgimenes <crg@crg.eti.br>
2024-04-23 23:51:55 -0300

    more vim-like commands

--- client/controller/inputhandler/handler.go
+++ client/controller/inputhandler/handler.go
@@ -8,6 +8,10 @@ import (
 )
 
 var AvailableCommands = [...]string{
+	":h",
+	":help",
+	":q",
+	":quit",
 	":logout",
 	":users",
 	":numberOfUsers",
@@ -19,6 +23,10 @@ const (
 	Users
 	NumberOfUsers
 	Commands
+	H
+	Help
+	Q
+	Quit
 )
 
 type InputHandler struct {
